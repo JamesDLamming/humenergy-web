922d3bf2add90799321c6c5823844b78b807c6ab
JamesDLamming <james@jameslamming.com>
2023-12-01 16:55:39 -0500

    added hubspot push

---
+++ api/submit-to-hubspot.js
@@ -0,0 +1,22 @@
+// api/submit-to-hubspot.js
+export default async (req, res) => {
+
+    const hubspotData = req.body
+
+    try {
+        const hubspotResponse = await fetch('https://api.hsforms.com/submissions/v3/integration/submit/Y44562555/84fcc0c1-de34-4cf6-9b93-ddf273823f1d', {
+            method: 'POST',
+            body: JSON.stringify(hubspotData),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const jsonResponse = await hubspotResponse.json();
+        res.status(hubspotResponse.status).json(jsonResponse);
+    } catch (error) {
+        res.status(500).json({ message: 'Error submitting to HubSpot', error });
+    }
+};
+
+
--- components/SignupForm.js
+++ components/SignupForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Button from './Button'; // Adjust the import path as needed
+import Button from './Button';
 
 const SignupForm = () => {
     const [formData, setFormData] = useState({
@@ -7,6 +7,7 @@ const SignupForm = () => {
         lastName: '',
         email: '',
     });
+    const [errors, setErrors] = useState({});
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -14,17 +15,92 @@ const SignupForm = () => {
             ...prevState,
             [name]: value
         }));
+        // Clear errors for a particular field when it's modified
+        if (errors[name]) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                [name]: null
+            }));
+        }
     };
 
-    const handleSubmit = (event) => {
+    const validateForm = () => {
+        let isValid = true;
+        let errors = {};
+
+        if (!formData.firstName.trim()) {
+            errors.firstName = 'First name is required';
+            isValid = false;
+        }
+
+        if (!formData.email.trim()) {
+            errors.email = 'Email is required';
+            isValid = false;
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            errors.email = 'Email is invalid';
+            isValid = false;
+        }
+
+        setErrors(errors);
+        return isValid;
+    };
+
+    const handleSubmit = async(event) => {
         event.preventDefault();
+        if (!validateForm()) {
+            console.log('Validation Failed', errors);
+            return;
+        }
         console.log('Form Submitted', formData);
-        // Additional form submission logic here
+        // Add data to hubspot
+        const hubspotData = {
+          fields: [
+              {
+                  name: "email",
+                  value: formData.email
+              },
+              {
+                  name: "firstname",
+                  value: formData.firstName
+              },
+              {
+                  name: "lastname",
+                  value: formData.lastName
+              }
+          ],
+          context: {
+              // "hutk": include HubSpot usertoken if necessary
+              "pageUri": window.location.href,
+              "pageName": document.title
+          }
+      };
+
+      try {
+        const response = await fetch('/api/submit-to-hubspot',  {
+            method: 'POST',
+            body: JSON.stringify(hubspotData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const jsonResponse = await response.json();
+
+        if (response.ok) {
+            console.log('Form submitted:', jsonResponse);
+            // Handle the response if necessary, such as showing a thank you message
+        } else {
+            console.error('Form submission error:', response);
+            // Handle HTTP errors
+        }
+    } catch (error) {
+        console.error('An unexpected error occurred:', error);
+        // Handle network errors
+    }
     };
 
     return (
         <div>
-            <form onSubmit={handleSubmit} className="space-y-4">
+            <form onSubmit={handleSubmit} noValidate className="space-y-4">
                 <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
                     <div className="flex-1">
                         <input
@@ -34,9 +110,10 @@ const SignupForm = () => {
                             placeholder="First Name"
                             value={formData.firstName}
                             onChange={handleChange}
-                            className="p-2 border rounded-md w-full"
+                            className={`p-2 border rounded-md w-full ${errors.firstName ? 'border-red-500' : ''}`}
                             required
                         />
+                        {errors.firstName && <p className="text-red-500 text-xs mt-1">{errors.firstName}</p>}
                     </div>
                     <div className="flex-1">
                         <input
@@ -48,6 +125,7 @@ const SignupForm = () => {
                             onChange={handleChange}
                             className="p-2 border rounded-md w-full"
                         />
+                        {/* Assuming last name isn't required, no error handling here */}
                     </div>
                 </div>
                 <div>
@@ -58,9 +136,10 @@ const SignupForm = () => {
                         placeholder="Email"
                         value={formData.email}
                         onChange={handleChange}
-                        className="p-2 border rounded-md w-full"
+                        className={`p-2 border rounded-md w-full ${errors.email ? 'border-red-500' : ''}`}
                         required
                     />
+                    {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email}</p>}
                 </div>
                 <Button text="Submit" type="submit" className="w-full" />
             </form>
@@ -69,3 +148,6 @@ const SignupForm = () => {
 };
 
 export default SignupForm;
+
+
+
