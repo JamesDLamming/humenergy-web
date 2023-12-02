9abb4c5742875b58b1ec07c5061e8f4d300ceae6
JamesDLamming <james@jameslamming.com>
2023-12-01 21:42:21 -0500

    submitted output from form

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -2,152 +2,167 @@ import React, { useState } from 'react';
 import Button from './Button';
 
 const SignupForm = () => {
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-    });
-    const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+  const [errors, setErrors] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-        // Clear errors for a particular field when it's modified
-        if (errors[name]) {
-            setErrors(prevErrors => ({
-                ...prevErrors,
-                [name]: null
-            }));
-        }
-    };
-
-    const validateForm = () => {
-        let isValid = true;
-        let errors = {};
-
-        if (!formData.firstName.trim()) {
-            errors.firstName = 'First name is required';
-            isValid = false;
-        }
-
-        if (!formData.email.trim()) {
-            errors.email = 'Email is required';
-            isValid = false;
-        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-            errors.email = 'Email is invalid';
-            isValid = false;
-        }
-
-        setErrors(errors);
-        return isValid;
-    };
-
-    const handleSubmit = async(event) => {
-        event.preventDefault();
-        if (!validateForm()) {
-            console.log('Validation Failed', errors);
-            return;
-        }
-        console.log('Form Submitted', formData);
-        // Add data to hubspot
-        const hubspotData = {
-          fields: [
-              {
-                  name: "email",
-                  value: formData.email
-              },
-              {
-                  name: "firstname",
-                  value: formData.firstName
-              },
-              {
-                  name: "lastname",
-                  value: formData.lastName
-              }
-          ],
-          context: {
-              // "hutk": include HubSpot usertoken if necessary
-              "pageUri": window.location.href,
-              "pageName": document.title
-          }
-      };
-
-      try {
-        const response = await fetch('/api/submit-to-hubspot',  {
-            method: 'POST',
-            body: JSON.stringify(hubspotData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-        const jsonResponse = await response.json();
-
-        if (response.ok) {
-            console.log('Form submitted:', jsonResponse);
-            // Handle the response if necessary, such as showing a thank you message
-        } else {
-            console.error('Form submission error:', response);
-            // Handle HTTP errors
-        }
-    } catch (error) {
-        console.error('An unexpected error occurred:', error);
-        // Handle network errors
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    // Clear errors for a particular field when it's modified
+    if (errors[name]) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: null,
+      }));
     }
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    let errors = {};
+
+    if (!formData.firstName.trim()) {
+      errors.firstName = 'First name is required';
+      isValid = false;
+    }
+
+    if (!formData.email.trim()) {
+      errors.email = 'Email is required';
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = 'Email is invalid';
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!validateForm()) {
+      console.log('Validation Failed', errors);
+      return;
+    }
+    console.log('Form Submitted', formData);
+    // Add data to hubspot
+    const hubspotData = {
+      fields: [
+        {
+          name: 'email',
+          value: formData.email,
+        },
+        {
+          name: 'firstname',
+          value: formData.firstName,
+        },
+        {
+          name: 'lastname',
+          value: formData.lastName,
+        },
+      ],
+      context: {
+        // "hutk": include HubSpot usertoken if necessary
+        pageUri: window.location.href,
+        pageName: document.title,
+      },
     };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit} noValidate className="space-y-4">
-                <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
-                    <div className="flex-1">
-                        <input
-                            type="text"
-                            id="firstName"
-                            name="firstName"
-                            placeholder="First Name"
-                            value={formData.firstName}
-                            onChange={handleChange}
-                            className={`p-2 border rounded-md w-full ${errors.firstName ? 'border-red-500' : ''}`}
-                            required
-                        />
-                        {errors.firstName && <p className="text-red-500 text-xs mt-1">{errors.firstName}</p>}
-                    </div>
-                    <div className="flex-1">
-                        <input
-                            type="text"
-                            id="lastName"
-                            name="lastName"
-                            placeholder="Last Name"
-                            value={formData.lastName}
-                            onChange={handleChange}
-                            className="p-2 border rounded-md w-full"
-                        />
-                        {/* Assuming last name isn't required, no error handling here */}
-                    </div>
-                </div>
-                <div>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className={`p-2 border rounded-md w-full ${errors.email ? 'border-red-500' : ''}`}
-                        required
-                    />
-                    {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email}</p>}
-                </div>
-                <Button text="Submit" type="submit" className="w-full" />
-            </form>
+    try {
+      const response = await fetch('/api/submit-to-hubspot', {
+        method: 'POST',
+        body: JSON.stringify(hubspotData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const jsonResponse = await response.json();
+
+      if (response.ok) {
+        console.log('Form submitted:', jsonResponse);
+        setIsSubmitted(true);
+        console.log('isSubmitted set to true');
+
+        // Handle the response if necessary, such as showing a thank you message
+      } else {
+        console.error('Form submission error:', response);
+        // Handle HTTP errors
+      }
+    } catch (error) {
+      console.error('An unexpected error occurred:', error);
+      // Handle network errors
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} noValidate className="space-y-4">
+        <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+          <div className="flex-1">
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+              className={`p-2 border rounded-md w-full ${
+                errors.firstName ? 'border-red-500' : ''
+              }`}
+              required
+            />
+            {errors.firstName && (
+              <p className="text-red-500 text-xs mt-1">{errors.firstName}</p>
+            )}
+          </div>
+          <div className="flex-1">
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+              className="p-2 border rounded-md w-full"
+            />
+            {/* Assuming last name isn't required, no error handling here */}
+          </div>
         </div>
-    );
+        <div>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            className={`p-2 border rounded-md w-full ${
+              errors.email ? 'border-red-500' : ''
+            }`}
+            required
+          />
+          {errors.email && (
+            <p className="text-red-500 text-xs mt-1">{errors.email}</p>
+          )}
+        </div>
+        <Button
+          text={isSubmitted ? 'Submitted' : 'Submit'}
+          type="submit"
+          className={`w-full ${
+            isSubmitted ? 'bg-green-500 text-white' : 'bg-blue-500 text-white'
+          }`}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SignupForm;
-
-
-
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
-import Button from '../components/Button'
+import Button from '../components/Button';
 import SignupForm from '../components/SignupForm';
-
-
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,43 +14,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-            
-          {!isModalOpen && (
-           <div className="bg-white p-8 rounded-lg shadow-md sm:w-9/12 w-11/12 h-1/3 items-center justify-center flex">
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        {!isModalOpen && (
+          <div className="bg-white p-8 rounded-lg shadow-md sm:w-9/12 w-11/12 h-1/3 items-center justify-center flex">
             <div>
-           <h1 className="text-2xl font-bold text-center mb-4">Welcome to <span className="text-blue-600">Hum</span></h1>
-           <div className="flex justify-center">
-             <Button text="Join the wailtlist" onClick={() => setIsModalOpen(true)} />
-           </div>
-         </div>
-         </div>
-          )}
-
-            {isModalOpen && (
-                <div className="modal relative bg-white shadow-md p-10 pt-12 rounded-lg md:w-9/12 w-11/12">
-
-                  <div className="absolute top-1.5 left-3.5  ">
-
-                    <button
-                        onClick={() => { setIsModalOpen(false) }}
-                        className="text-2xl font-semibold"
-                    >
-                        &times;
-                    </button>
+              <h1 className="text-2xl font-bold text-center mb-4">
+                Welcome to <span className="text-blue-600">Hum</span>
+              </h1>
+              <div className="flex justify-center">
+                <Button
+                  text="Join the wailtlist"
+                  onClick={() => setIsModalOpen(true)}
+                />
+              </div>
             </div>
-                    <SignupForm />
-                </div>
-            )}
+          </div>
+        )}
 
-
-
+        {isModalOpen && (
+          <div className="modal relative bg-white shadow-md p-10 pt-12 rounded-lg md:w-9/12 w-11/12">
+            <div className="absolute top-1.5 left-3.5  ">
+              <button
+                onClick={() => {
+                  setIsModalOpen(false);
+                }}
+                className="text-2xl font-semibold"
+              >
+                &times;
+              </button>
+            </div>
+            <SignupForm />
+          </div>
+        )}
+      </div>
     </div>
-    
-    </div>
-
-    
   );
 }
-
