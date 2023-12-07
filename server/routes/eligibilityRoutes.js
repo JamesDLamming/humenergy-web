be486122d6359f145c168f7649fe06a69c799554
JamesDLamming <james@jameslamming.com>
2023-12-07 18:34:04 -0500

    reverse url change

--- server/routes/eligibilityRoutes.js
+++ server/routes/eligibilityRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express();
+const router = express.Router();
 const { checkEligibility } = require('../controllers/eligibilityController');
 
 router.post('/check-eligibility', checkEligibility);
