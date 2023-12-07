ccfa14ccbefdb8f678c1dfdbc064ae59393749d2
JamesDLamming <james@jameslamming.com>
2023-12-07 18:51:27 -0500

    test

--- server/routes/eligibilityRoutes.js
+++ server/routes/eligibilityRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express();
 const { checkEligibility } = require('../controllers/eligibilityController');
 
 router.post('/check-eligibility', checkEligibility);
