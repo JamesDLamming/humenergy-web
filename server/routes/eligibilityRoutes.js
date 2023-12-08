94623e693c448617bccb6d083b362b8502113c24
JamesDLamming <james@jameslamming.com>
2023-12-07 19:33:30 -0500

    URL test update

--- server/routes/eligibilityRoutes.js
+++ server/routes/eligibilityRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const router = express.Router();
-const router = express();
+const router = express.Router();
+//const router = express();
 const { checkEligibility } = require('../controllers/eligibilityController');
 
 //router.post('/check-eligibility', checkEligibility);
