70fa678ab5e940352ea6f38982401b5c313b4212
JamesDLamming <james@jameslamming.com>
2023-12-07 19:23:55 -0500

    url

--- server/routes/eligibilityRoutes.js
+++ server/routes/eligibilityRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { checkEligibility } = require('../controllers/eligibilityController');
 
-router.post('/check-eligibility', checkEligibility);
+//git purouter.post('/check-eligibility', checkEligibility);
 router.post('/test', (req, res) => {
   res.json({ message: 'Test endpoint reached' });
 });
