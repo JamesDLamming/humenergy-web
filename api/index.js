c9af5408a4173cf8a22c242859d936c936b82d3d
JamesDLamming <james@jameslamming.com>
2023-12-08 13:28:36 -0500

    moved test api

--- api/index.js
+++ api/index.js
@@ -10,6 +10,12 @@ app.use(cors());
 
 app.use(eligibilityRoutes);
 
+app.use(
+  router.post('/api/test2', (req, res) => {
+    res.json({ message: 'Test endpoint reached' });
+  })
+);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
