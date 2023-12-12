5c7508bab30bac7d6c3098741c2d71c14936ec0f
JamesDLamming <james@jameslamming.com>
2023-12-12 15:01:24 -0500

    routed api correctly

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 
 const express = require('express');
 const eligibilityRoutes = require('./routes/eligibilityRoutes');
+const findUtilitiesFromState = require('./routes/findUtilitiesFromStateRoutes');
 
 const app = express();
 app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
-app.use(eligibilityRoutes);
+app.use(eligibilityRoutes, findUtilitiesFromState);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
