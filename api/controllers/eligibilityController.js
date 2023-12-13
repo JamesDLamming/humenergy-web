f3b33077b9850dbde668bcf01d8306728934bda2
JamesDLamming <james@jameslamming.com>
2023-12-13 17:08:29 -0500

    filter on DERs

--- api/controllers/eligibilityController.js
+++ api/controllers/eligibilityController.js
@@ -3,11 +3,65 @@ const { accessSpreadsheet } = require('../services/googleSheetsService');
 
 async function checkEligibility(req, res) {
   try {
-    const { stateRegion, sectorOption, Utility } = req.body;
+    const {
+      stateRegion,
+      sectorOption,
+      Utility,
+      selectionsThermostat,
+      selectionsBattery,
+      heatpumpPresent,
+      waterheaterPresent,
+      solarPresent,
+      EVPresent,
+      generatorPresent,
+    } = req.body;
 
     const doc = await accessSpreadsheet();
     const sheet = doc.sheetsByIndex[0]; // or use sheetsById or sheetsByTitle
     const rows = await sheet.getRows();
+
+    let smartThermostat = '';
+    let battery = '';
+    let powerwall = '';
+    let heatpump = '';
+    let waterheater = '';
+    let EV = '';
+    let solar = '';
+    let generator = '';
+
+    selectionsThermostat.NoThermostat === true
+      ? (smartThermostat = '')
+      : (smartThermostat = 'Thermostats');
+    selectionsBattery.NoBattery === true
+      ? (battery = '')
+      : (battery = 'Batteries');
+    selectionsBattery.TeslaPowerwall === true
+      ? (powerwall = 'Powerwall')
+      : (powerwall = '');
+
+    heatpumpPresent === true ? (heatpump = 'Heat Pumps') : (heatpump = '');
+    waterheaterPresent === true
+      ? (waterheater = 'Water Heaters')
+      : (waterheater = '');
+    EVPresent === true ? (EV = 'EVs') : (EV = '');
+    solarPresent === true ? (solar = 'Solar') : (solar = '');
+    generatorPresent === true ? (generator = 'Generators') : (generator = '');
+
+    console.log(selectionsBattery.NoBattery, selectionsBattery.TeslaPowerwall);
+    console.log(battery, powerwall, solar, smartThermostat);
+
+    const derArray = [
+      smartThermostat,
+      battery,
+      powerwall,
+      heatpump,
+      waterheater,
+      EV,
+      solar,
+      generator,
+    ];
+
+    console.log(derArray);
 
     const filteredRows = rows.filter(
       (row) =>
@@ -15,7 +69,11 @@ async function checkEligibility(req, res) {
         (row.get('Sector').includes(sectorOption) ||
           row.get('Sector') === '') &&
         row.get('Status') != 'Ended' &&
-        row.get('Utility/CCA') === Utility
+        row.get('Utility/CCA') === Utility &&
+        row
+          .get('DERs')
+          .split(', ')
+          .some((der) => derArray.includes(der)) // split DERs and check if present in derArray
     );
     res.json(
       filteredRows.map((row) => {
--- pages/VPPFinder.js
+++ pages/VPPFinder.js
@@ -13,6 +13,16 @@ export default function VPPFinder() {
   const [tableVisible, setTableVisible] = useState(false);
   const [utilityVisible, setUtilityVisible] = useState(false);
   const [propertyTypeVisible, setPropertyTypeVisible] = useState(false);
+  const [devicesVisible, setDevicesVisible] = useState(false);
+  const [heatpumpPresent, setHeatpumpPresent] = useState(false);
+
+  const [waterheaterPresent, setWaterheaterPresent] = useState(false);
+
+  const [EVPresent, setEVPresent] = useState(false);
+
+  const [solarPresent, setSolarPresent] = useState(false);
+
+  const [generatorPresent, setGeneratorPresent] = useState(false);
 
   const [loading, setLoading] = useState(false);
   const [utilityState, setUtilityState] = useState('');
@@ -24,7 +34,6 @@ export default function VPPFinder() {
   const [propertyTypeError, setPropertyTypeError] = useState('');
 
   const [stateError, setStateError] = useState('');
-  console.log('environemtn', process.env.NEXT_PUBLIC_BASE_API_URL);
   const getUtilities = async (stateRegion, sectorOption) => {
     try {
       const response = await fetch(
@@ -58,7 +67,18 @@ export default function VPPFinder() {
             'Content-Type': 'application/json',
           },
           // Include any necessary request body here
-          body: JSON.stringify({ stateRegion, sectorOption, Utility }),
+          body: JSON.stringify({
+            stateRegion,
+            sectorOption,
+            Utility,
+            selectionsThermostat,
+            selectionsBattery,
+            heatpumpPresent,
+            waterheaterPresent,
+            solarPresent,
+            EVPresent,
+            generatorPresent,
+          }),
         }
       );
       const jsonResponse = await response.json();
@@ -91,7 +111,58 @@ export default function VPPFinder() {
     setTableVisible(true);
   };
 
-  const [sectorOption, setSectorOption] = React.useState('');
+  const [sectorOption, setSectorOption] = useState('');
+
+  const [selectionsThermostat, setSelectionsThermostat] = useState({
+    Honeywell: false,
+    Nest: false,
+    OtherThermostat: false,
+    NoThermostat: true, // 'None' option selected by default
+  });
+
+  const [selectionsBattery, setSelectionsBattery] = useState({
+    Generac: false,
+    TeslaPowerwall: false,
+    OtherBattery: false,
+    NoBattery: true, // 'None' option selected by default
+  });
+
+  const handleCheckboxChangeThermostat = (event) => {
+    setSelectionsThermostat({
+      ...selectionsThermostat,
+      [event.target.name]: event.target.checked,
+      NoThermostat: false, // Uncheck 'None' when any checkbox is checked
+    });
+  };
+
+  const handleCheckboxChangeBattery = (event) => {
+    setSelectionsBattery({
+      ...selectionsBattery,
+      [event.target.name]: event.target.checked,
+      NoBattery: false, // Uncheck 'None' when any checkbox is checked
+    });
+  };
+
+  // Handle 'None' radio button change
+  const handleNoneChangeThermostat = () => {
+    setSelectionsThermostat({
+      Honeywell: false,
+      Nest: false,
+      OtherThermostat: false,
+
+      NoThermostat: true,
+    });
+  };
+
+  const handleNoneChangeBattery = () => {
+    setSelectionsBattery({
+      Generac: false,
+      TeslaPowerwall: false,
+      OtherBattery: false,
+      NoBattery: true,
+    });
+  };
+
   useEffect(() => {
     setUtility('');
   }, [stateRegion, sectorOption]);
@@ -134,7 +205,7 @@ export default function VPPFinder() {
                   <div className="font-semibold w-1/3">Property Type:</div>
                   <div className="w-2/3">
                     <div>
-                      <label className="">
+                      <label className="flex items-baseline">
                         <input
                           type="radio"
                           name="propertyType"
@@ -156,12 +227,12 @@ export default function VPPFinder() {
                       </label>
                     </div>
                     <div>
-                      <label className="">
+                      <label className="flex items-baseline">
                         <input
                           type="radio"
                           name="propertyType"
-                          value="Multi-family"
-                          checked={sectorOption === 'Multi-family'}
+                          value="Multifamily"
+                          checked={sectorOption === 'Multifamily'}
                           onChange={(e) => {
                             const newSectorOption = e.target.value;
                             setSectorOption(newSectorOption);
@@ -175,7 +246,7 @@ export default function VPPFinder() {
                       </label>
                     </div>
                     <div>
-                      <label className="">
+                      <label className="flex items-baseline">
                         <input
                           type="radio"
                           name="propertyType"
@@ -214,7 +285,7 @@ export default function VPPFinder() {
                       const newUtiltyValue = e.target.value;
                       setUtility(newUtiltyValue);
                       setUtilityError('');
-
+                      setDevicesVisible(true);
                       setTableVisible(false);
                     }}
                   />
@@ -225,7 +296,235 @@ export default function VPPFinder() {
                   {utilityError}
                 </div>
               )}
+              {devicesVisible && Utility != 'Unavailable' && (
+                <label className="mt-2 block gap-x-3 items-top">
+                  <div className="text-xl pt-2 font-black items-center text-center">
+                    Add devices
+                  </div>
+                  <div className="block">
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Smart Thermostat</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <label className="flex items-baseline">
+                          <input
+                            type="checkbox"
+                            name="Honeywell"
+                            checked={selectionsThermostat.Honeywell}
+                            onChange={handleCheckboxChangeThermostat}
+                          />
+                          <span className="ml-1 ">Honeywell</span>
+                        </label>
+                        <label className="flex items-baseline">
+                          <input
+                            type="checkbox"
+                            name="Nest"
+                            checked={selectionsThermostat.Nest}
+                            onChange={handleCheckboxChangeThermostat}
+                          />
+                          <span className="ml-1">Nest</span>
+                        </label>
+                        <label className="flex items-baseline">
+                          <input
+                            type="checkbox"
+                            name="OtherThermostat"
+                            checked={selectionsThermostat.OtherThermostat}
+                            onChange={handleCheckboxChangeThermostat}
+                          />
+                          <span className="ml-1">Other</span>
+                        </label>
+                        <label className="flex items-baseline">
+                          <input
+                            type="radio"
+                            name="NoThermostat"
+                            checked={selectionsThermostat.NoThermostat}
+                            onChange={handleNoneChangeThermostat}
+                          />
+                          <span className="ml-1">None</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Battery</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <div>
+                          <label className="flex items-baseline">
+                            <input
+                              type="checkbox"
+                              name="Generac"
+                              checked={selectionsBattery.Generac}
+                              onChange={handleCheckboxChangeBattery}
+                            />
+                            <span className="ml-1">Generac</span>
+                          </label>
+                        </div>
+                        <div>
+                          <label className="flex items-baseline">
+                            <input
+                              type="checkbox"
+                              name="TeslaPowerwall"
+                              checked={selectionsBattery.TeslaPowerwall}
+                              onChange={handleCheckboxChangeBattery}
+                            />
+                            <span className="ml-1">Tesla Powerwall</span>
+                          </label>
+                        </div>
+                        <div>
+                          <label className="flex items-baseline">
+                            <input
+                              type="checkbox"
+                              name="OtherBattery"
+                              checked={selectionsBattery.OtherBattery}
+                              onChange={handleCheckboxChangeBattery}
+                            />
+                            <span className="ml-1">Other</span>
+                          </label>
+                        </div>
+                        <div>
+                          <label className="flex items-baseline">
+                            <input
+                              type="radio"
+                              name="NoBattery"
+                              checked={selectionsBattery.NoBattery}
+                              onChange={handleNoneChangeBattery}
+                            />
+                            <span className="ml-1">None</span>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Heat Pump</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <label className="flex items-center">
+                          <input
+                            type="radio"
+                            name="heatpump"
+                            value="heatpumpPresent"
+                            checked={heatpumpPresent === true}
+                            onChange={() => setHeatpumpPresent(true)}
+                          />
+                          <span className="ml-2">Yes</span>
+                        </label>
 
+                        <label className="flex items-center">
+                          <input
+                            type="radio"
+                            name="heatpump"
+                            value="noHeatpumpPresent"
+                            checked={heatpumpPresent === false}
+                            onChange={() => setHeatpumpPresent(false)}
+                          />
+                          <span className="ml-1">None</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Electric Water Heater</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <label className="flex items-center">
+                          <input
+                            type="radio"
+                            name="waterheater"
+                            value="waterheaterPresent"
+                            checked={waterheaterPresent === true}
+                            onChange={() => setWaterheaterPresent(true)}
+                          />
+                          <span className="ml-2">Yes</span>
+                        </label>
+
+                        <label className="flex items-center">
+                          <input
+                            type="radio"
+                            name="waterheater"
+                            value="nowaterheaterPresent"
+                            checked={waterheaterPresent === false}
+                            onChange={() => setWaterheaterPresent(false)}
+                          />
+                          <span className="ml-1">None</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Solar</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <label className="">
+                          <input
+                            type="radio"
+                            name="solarPresent"
+                            value="solarPresent"
+                            checked={solarPresent === true}
+                            onChange={() => setSolarPresent(true)}
+                          />
+                          <span className="ml-2">Yes</span>
+                        </label>
+
+                        <label className="flex items-baseline">
+                          <input
+                            type="radio"
+                            name="solar"
+                            value="noSolarPresent"
+                            checked={solarPresent === false}
+                            onChange={() => setSolarPresent(false)}
+                          />
+                          <span className="ml-1">None</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Electric Vehicle</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <label className="">
+                          <input
+                            type="radio"
+                            name="EVPresent"
+                            value="EVPresent"
+                            checked={EVPresent === true}
+                            onChange={() => setEVPresent(true)}
+                          />
+                          <span className="ml-2">Yes</span>
+                        </label>
+
+                        <label className="flex items-baseline">
+                          <input
+                            type="radio"
+                            name="EV"
+                            value="noEVPresent"
+                            checked={EVPresent === false}
+                            onChange={() => setEVPresent(false)}
+                          />
+                          <span className="ml-1">None</span>
+                        </label>
+                      </div>
+                    </div>
+                    <div className="mt-2">
+                      <p className="mb-0 font-medium">Generator</p>
+                      <div className="flex gap-x-3 gap-y-1 flex-wrap">
+                        <label className="">
+                          <input
+                            type="radio"
+                            name="generatorPresent"
+                            value="generatorPresent"
+                            checked={generatorPresent === true}
+                            onChange={() => setGeneratorPresent(true)}
+                          />
+                          <span className="ml-2">Yes</span>
+                        </label>
+
+                        <label className="flex items-baseline">
+                          <input
+                            type="radio"
+                            name="generator"
+                            value="noGeneratorPresent"
+                            checked={generatorPresent === false}
+                            onChange={() => setGeneratorPresent(false)}
+                          />
+                          <span className="ml-1">None</span>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </label>
+              )}
               <DefaultButton
                 type="submit"
                 className={
