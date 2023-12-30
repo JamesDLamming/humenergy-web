6d7773d25947d3edc53735a0bf2af6d3c0704f79
JamesDLamming <james@jameslamming.com>
2023-12-29 22:29:42 -0500

    about paragraph grid

--- pages/about.js
+++ pages/about.js
@@ -18,26 +18,24 @@ const AboutUsPage = () => {
             <header className="text-center my-10">
               <h1 className="text-4xl font-bold text-accent">About Us</h1>
             </header>
-            <div className="max-w-xl px-4 mx-auto gap-y-2 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24 relative z-10 mb-10">
-              <p className="mb-2">
+            <div className="max-w-xl px-4 mx-auto grid gap-6 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid-cols-2 lg:gap-24 relative z-10 mb-10">
+              <div className="">
                 At Hum Energy, we believe your energy should work for you.
                 That's why we help you understand what is going on with your
                 electricity use, and automatically optimize it, so you can save
                 money and reduce your carbon footprint, all without impacting
                 the way you live your life. We help you to save money - and earn
                 it! - without lifting a finger.
-              </p>
-              <p>
+              </div>
+              <div>
                 By bringing all your devices under one roof in the Hum App,
                 you're able to have a complete picture of what is going on with
                 your energy use, find new ways to reduce it, and even find ways
                 to earn money from your devices.
-                <br></br>
-                <br></br>
-                <span className="font-bold text-lg italic">
-                  Take control of your energy and start saving today
-                </span>
-              </p>
+              </div>
+              <div className="font-bold text-lg italic">
+                Take control of your energy and start saving today
+              </div>
             </div>
             <div className=" text-xl mb-3 flex flex-wrap items-center justify-center lg:flex-1 gap-x-2">
               <img
