257395ab2ded2137d711f571bda061b28205d548
JamesDLamming <james@jameslamming.com>
2024-01-08 16:22:06 -0500

    no submission on local host

--- api/controllers/saveFormDataController.js
+++ api/controllers/saveFormDataController.js
@@ -2,14 +2,22 @@ const { json } = require('express');
 const { accessSpreadsheet } = require('../services/googleSheetsService');
 
 async function saveFormData(req, res) {
-  try {
-    const doc = await accessSpreadsheet();
-    const sheet = doc.sheetsByTitle['formData'];
-    const response = await sheet.addRow(req.body);
-    res.json(response.data); // Send back the response data to the client
-  } catch (error) {
-    console.error('Error in saveFormData:', error);
-    res.status(500).json({ error: error.message }); // Send detailed error message
+  console.log(req.ip);
+  const clientIP = req.ip; // or use req.headers['x-forwarded-for'] if behind a proxy
+  const myIP = '::1'; // Replace with your IP address; currently setup for avoiding local host submission requirements
+  if (clientIP === myIP) {
+    console.log('Form submission from my IP, not saving data.');
+    res.status(200).send('Not saved due to IP match');
+  } else {
+    try {
+      const doc = await accessSpreadsheet();
+      const sheet = doc.sheetsByTitle['formData'];
+      const response = await sheet.addRow(req.body);
+      res.json(response.data); // Send back the response data to the client
+    } catch (error) {
+      console.error('Error in saveFormData:', error);
+      res.status(500).json({ error: error.message }); // Send detailed error message
+    }
   }
 }
 
